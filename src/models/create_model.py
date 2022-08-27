c5a025cfcfd1e27f0bd39de6510e166371b12a34
ashen iranga hewarathna <asheniranga@gmail.com>
2022-08-27 12:11:41 +0530

    fit estimator class update and fix type checking error bug

---
+++ src/models/create_model.py
@@ -0,0 +1,171 @@
+import numpy as np
+from sklearn.model_selection import KFold, RandomizedSearchCV, GridSearchCV
+
+
+class FitEstimator:
+    """fitting estimators"""
+
+    def __init__(self, estimator, random_state):
+        self.best_random_est = None
+        self._target = None
+        self._fold_dtl = None
+        self._est = estimator
+        self.RANDOM_STATE = random_state
+
+    def create_folds(self, k=5, shuffle=True):
+        return KFold(n_splits=k, random_state=self.RANDOM_STATE, shuffle=shuffle)
+
+    def fit(self, x, y):
+        self._est.fit(x, y)
+
+    def refit(self, x, y):
+        self.best_random_est.fit(x, y)
+
+    def predict(self, x):
+        return self._est.predict(x)
+
+    @staticmethod
+    def score(true_y, pred_y, method, squared=False):
+        if not squared:
+            return np.sqrt(method(true_y, pred_y))
+        else:
+            return method(true_y, pred_y)
+
+    def fit_cv(self, x, y, scoring, test_x, test_y=None, k=5):
+        folds = self.create_folds(k=k)
+        self._fold_dtl = {}
+        self._target = np.zeros(len(test_x))
+        i = 0
+
+        for t, v in folds.split(x, y):
+            self.fit(x.iloc[t], y.iloc[t])
+            train_pred = self.predict(x.iloc[t])
+            valid_pred = self.predict(x.iloc[v])
+
+            train_score = self.score(y.iloc[t], train_pred, scoring)
+            valid_score = self.score(y.iloc[v], valid_pred, scoring)
+
+            if test_y is not None:
+                test_pred = self.predict(test_x)
+                test_score = self.score(test_y, test_pred, scoring)
+
+                self._fold_dtl[f'fold-{i}'] = {'train_score': train_score,
+                                               'validation_score': valid_score,
+                                               'test_score': test_score}
+
+            else:
+                test_pred = self.predict(test_x)
+                self._target += test_pred
+
+                self._fold_dtl[f'fold-{i}'] = {'train_score': train_score,
+                                               'validation_score': valid_score}
+            i += 1
+
+    @staticmethod
+    def re_arrange_para_dist(factor, best_vales):
+        new_param = {}
+
+        for key, value in best_vales.items():
+            if not isinstance(value, str):
+                if isinstance(value, (int, np.int32, np.int64, np.int16, np.int8)):
+                    boundary = value // factor
+                else:
+                    boundary = value / factor
+
+                lower = value - boundary
+                upper = value + boundary
+
+                if lower < 0:
+                    if isinstance(value, (int, np.int32, np.int64, np.int16, np.int8)):
+                        paras = []
+
+                        while len(paras) != 3:
+                            p = np.random.randint(0, upper, 1)[0]
+
+                            if p not in paras:
+                                paras.append(p)
+
+                        new_param[key] = paras
+
+                    elif isinstance(value, (float, np.float16, np.float32, np.float64)):
+                        paras = []
+
+                        while len(paras) != 3:
+                            p = np.random.uniform(0, upper, 1)[0]
+
+                            if p not in paras:
+                                paras.append(p)
+
+                        new_param[key] = paras
+
+                else:
+                    if isinstance(value, (int, np.int32, np.int64, np.int16, np.int8)):
+                        paras = []
+
+                        while len(paras) != 3:
+                            p = np.random.randint(lower, upper, 1)[0]
+
+                            if p not in paras:
+                                paras.append(p)
+
+                        new_param[key] = paras
+
+                    elif isinstance(value, (float, np.float16, np.float32, np.float64)):
+                        paras = []
+
+                        while len(paras) != 3:
+                            p = np.random.uniform(lower, upper, 1)[0]
+
+                            if p not in paras:
+                                paras.append(p)
+
+                        new_param[key] = paras
+            else:
+                new_param[key] = value
+
+        return new_param
+
+    def random_search(self, param_distributions, scoring, n_iter, cv, verbose):
+        return RandomizedSearchCV(estimator=self._est,
+                                  param_distributions=param_distributions,
+                                  scoring=scoring,
+                                  n_iter=n_iter,
+                                  cv=cv,
+                                  n_jobs=-1,
+                                  verbose=verbose)
+
+    def grid_search(self, param_grid, scoring, cv, verbose):
+        return GridSearchCV(estimator=self._est,
+                            param_grid=param_grid,
+                            scoring=scoring,
+                            cv=cv,
+                            n_jobs=-1,
+                            verbose=verbose)
+
+    def fine_tune(self, x, y, init_grid, n_random_tunes, factor=2, cv=None, n_iter=10, scoring=None, grid_search=True):
+        rs = self.random_search(param_distributions=init_grid,
+                                scoring=scoring,
+                                n_iter=n_iter,
+                                cv=cv,
+                                verbose=1).fit(x, y)
+
+        current_para = rs.best_params_
+        self.best_random_est = rs.best_estimator_
+
+        for _ in range(n_random_tunes):
+            re_para_dis = self.re_arrange_para_dist(factor, current_para)
+
+            temp = self.random_search(param_distributions=re_para_dis,
+                                      scoring=scoring,
+                                      n_iter=n_iter,
+                                      cv=cv,
+                                      verbose=1).fit(x, y)
+
+            current_para = temp.best_params_
+            self.best_random_est = temp.best_estimator_
+
+        if grid_search:
+            return
+
+        return self.best_random_est
+
