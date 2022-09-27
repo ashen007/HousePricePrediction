a755f261f3189e68f7c4cde7c42932e08d6e982b
ashen iranga hewarathna <asheniranga@gmail.com>
2022-09-27 12:09:00 +0530

    full feature improve pipeline

--- src/features/v3/improve_pipeline.py
+++ src/features/v3/improve_pipeline.py
@@ -2,23 +2,51 @@ import numpy as np
 import pandas as pd
 from feature_engine.imputation import EndTailImputer
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import SelectFromModel
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.preprocessing import FunctionTransformer
+from sklearn.preprocessing import FunctionTransformer, OneHotEncoder
+
+nominal_columns = ['MSZoning', 'Alley', 'LotShape', 'LandContour', 'Utilities',
+                   'LotConfig', 'Neighborhood', 'Condition1',
+                   'Condition2', 'BldgType', 'HouseStyle', 'Street',
+                   'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd',
+                   'MasVnrType', 'Foundation', 'Heating', 'Electrical',
+                   'Functional', 'BsmtFinType1', 'BsmtFinType2', 'GarageFinish',
+                   'GarageType', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition']
+
+ordinal_columns = ['MSSubClass', 'LandSlope', 'OverallQual',
+                   'OverallCond', 'ExterQual', 'ExterCond', 'BsmtQual',
+                   'BsmtCond', 'BsmtExposure', 'HeatingQC', 'KitchenQual',
+                   'FireplaceQu', 'GarageQual', 'GarageCond', 'PoolQC',
+                   'PavedDrive', 'CentralAir']
+
+continues_columns = ['LotFrontage', 'LotArea', 'MasVnrArea', 'BsmtFinSF1',
+                     'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF',
+                     '2ndFlrSF', 'LowQualFinSF', 'GrLivArea', 'GarageArea',
+                     'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', '3SsnPorch',
+                     'ScreenPorch', 'PoolArea', 'MiscVal']
+
+discrete_columns = ['YearBuilt', 'YearRemodAdd', 'BsmtFullBath', 'BsmtHalfBath',
+                    'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd',
+                    'Fireplaces', 'GarageYrBlt', 'GarageCars', 'MoSold', 'YrSold']
+
+missing_cat_columns = ['Alley', 'MasVnrType', 'BsmtQual', 'BsmtCond',
+                       'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2',
+                       'FireplaceQu', 'GarageType', 'GarageFinish',
+                       'GarageQual', 'GarageCond', 'PoolQC',
+                       'Fence', 'MiscFeature']
+
+missing_num_columns = ['LotFrontage', 'GarageYrBlt', 'MasVnrArea']
+
+imputing_cols = [*missing_cat_columns,
+                 'Electrical', 'LotFrontage',
+                 'MSZoning', 'MasVnrArea',
+                 'MasVnrType', 'GarageYrBlt']
 
 
 def imputation_pipeline():
-    missing_cat_columns = ['Alley', 'MasVnrType', 'BsmtQual', 'BsmtCond',
-                           'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2',
-                           'FireplaceQu', 'GarageType', 'GarageFinish',
-                           'GarageQual', 'GarageCond', 'PoolQC',
-                           'Fence', 'MiscFeature']
-    missing_num_columns = ['LotFrontage', 'GarageYrBlt', 'MasVnrArea']
-    imputing_cols = [*missing_cat_columns,
-                     'Electrical', 'LotFrontage',
-                     'MSZoning', 'MasVnrArea',
-                     'MasVnrType', 'GarageYrBlt']
-
     categorical_impute_pipeline_1 = Pipeline([
         ('categorical_features', FunctionTransformer(lambda df: df[missing_cat_columns])),
         ('mnar_impute', SimpleImputer(strategy='constant', fill_value='None'))
@@ -61,6 +89,36 @@ def imputation_pipeline():
 
     return Pipeline([('concat_pipes', FeatureUnion([('cat_imputer', cat_full_pipes),
                                                     ('num_imputer', num_full_pipes)]))])
+
+
+def improvement_pipeline(dataframe):
+    full_impute_pipeline = imputation_pipeline()
+    dataframe[imputing_cols] = full_impute_pipeline.fit_transform(dataframe)
+    order_map = {'Ex': 5, 'Gd': 4, 'TA': 3, 'Fa': 2, 'Po': 1, 'None': -1,
+                 'NO': -1, 'No': -1, 'Av': 3, 'Mn': 2, 'Reg': 0, 'IR1': 1,
+                 'IR2': 2, 'IR3': 3, 'Gtl': 1, 'Mod': 2, 'Sev': 3, 'Y': 1,
+                 'N': 0, 'P': -1}
+
+    nominal_encode_pipeline = Pipeline([('select_nominal', FunctionTransformer(lambda df: df[nominal_columns])),
+                                        ('one_hot', OneHotEncoder()),
+                                        (
+                                            'feature_selection',
+                                            SelectFromModel(RandomForestRegressor(min_samples_split=20,
+                                                                                  min_samples_leaf=10,
+                                                                                  n_jobs=-1))),
+                                        ('to_df',
+                                         FunctionTransformer(
+                                             lambda metrix: pd.DataFrame.sparse.from_spmatrix(metrix)))])
+
+    ordinal_encode_pipeline = Pipeline([('select_ordinal', FunctionTransformer(lambda df: df[ordinal_columns])),
+                                        ('encode', FunctionTransformer(lambda df: df.replace(order_map)))])
+
+    continues_pipeline = Pipeline([('select_cont', FunctionTransformer(lambda df: df[continues_columns])),
+                                   ('transformation', FunctionTransformer(lambda df: df.apply(np.log1p)))])
+
+    discrete_pipeline = Pipeline([('select_discrete', FunctionTransformer(lambda df: df[discrete_columns]))])
+
+    return
 
 
 class InfraredMedianImputer(BaseEstimator, TransformerMixin):
